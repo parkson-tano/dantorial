8a2c80b5d39a1a4c30baf2899b332ecf3bf179f1
King Dani <tanoparksonsilencer@gmail.com>
2021-08-26 10:44:43 +0100

    create review form

--- review/forms.py
+++ review/forms.py
@@ -1,3 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import *
+
+class ReviewForm(admin.ModelAdmin):
+  class Meta:
+    model = Review
+    fields = ('content', 'rating')
