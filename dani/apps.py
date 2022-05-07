8edaa59944a59381c358dde28b6c389b18cb2127
King Dani <tanoparksonsilencer@gmail.com>
2022-05-07 13:55:08 +0100

    init

--- dani/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DaniConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dani'
--- dani/dani_processor.py
+++
@@ -1,8 +0,0 @@
-from .models import Carousel
-from mainapp.models import Subject, ProfilePersonal
-def carousel_slide(request):
-    return {
-        'carousel' : Carousel.objects.all(),
-        'subject_res' : Subject.objects.all(),
-        'suggested': ProfilePersonal.objects.filter(paid=True)
-     }
