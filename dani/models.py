f392ab15f3d426b7b7e66cbfa022388bc897baa7
King Dani <tanoparksonsilencer@gmail.com>
2022-05-10 06:33:50 +0100

    init

--- dani/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Carousel(models.Model):
-    image = models.ImageField(upload_to='images/carousel')
-    text  = models.TextField()
-
